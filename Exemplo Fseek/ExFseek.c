cb55a4594f33e53713a435b3c8c920df01e24184
Wellington <wellingtondocarmodesouza@gmail.com>
2021-01-18 19:26:41 -0300

    Create ExFseek.c
    Me ajuda Resolve isso e está parecenco um erro arquivo.

---
+++ Exemplo Fseek/ExFseek.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct cadastro{
+
+   char nome[20], rua[20];
+   int idade;
+
+ };
+
+int main(){
+
+  FILE *f;
+  f = fopen("arquivo.txt","rb");
+
+  if(f==NULL){
+    printf("\nErro na abertura!!!\n");
+    system("pause");
+    exit(1);
+
+  }
+
+  struct cadastro c;
+  fseek(f,2*sizeof(struct cadastro),SEEK_SET);
+  fread(&c,sizeof(struct cadastro),1,f);
+  printf("%s\n%s\n%d\n",c.nome,c.rua,c.idade);
+
+
+  fclose(f);
+  system("pause");
+
+return 0;
+
+}
